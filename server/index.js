1558305e00b5bdf64e3a9c5fa809fb873938d565
Abdul Mannan <mannanibbrahim321@gmail.com>
2026-05-23 18:33:58 +0500

    chore: bind backend to 0.0.0.0 for Railway routing

--- server/index.js
+++ server/index.js
@@ -543,6 +543,6 @@ app.post("/api/storage/signed-url", authenticateUser, async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
