6c8312a5a984e05888a435505bff7788ff7b7701
Abdul Mannan <mannanibbrahim321@gmail.com>
2026-06-11 17:14:14 +0500

    feat: update email validation to accept digits 0-9 at start

--- MentorLink-main/src/Screens/Authentication/Auth.tsx
+++ MentorLink-main/src/Screens/Authentication/Auth.tsx
@@ -23,8 +23,8 @@ function Auth({ onClose }: Props) {
 
   const navigate = useNavigate();
 
-  const EMAIL_PATTERN = /^(2[0-9])ntucsfl\d{4}@student\.ntu\.edu\.pk$/;
-  const EXAMPLE_EMAIL = "23ntucsfl1003@student.ntu.edu.pk";
+  const EMAIL_PATTERN = /^[0-9]ntucsfl\d{4}@student\.ntu\.edu\.pk$/;
+  const EXAMPLE_EMAIL = "20ntucsfl1003@student.ntu.edu.pk";
 
   function isValidStudentEmail(email: string) {
     return EMAIL_PATTERN.test(email);
